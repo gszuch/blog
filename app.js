ca96b8b950e8444eab591e777a1d0517046f9938
Greg S <gszuch@gmail.com>
2018-03-06 14:01:59 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose");
+ 
+// APP CONFIG
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// MONGOOSE MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+// RESTFUL ROUTES
+
+app.get("/", function(req, res){
+    res.redirect("/blogs");
+});
+
+// INDEX ROUTE
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log(err);
+        }else{
+           res.render("index", {blogs: blogs}); 
+        }
+    });
+    
+});
+
+// NEW ROUTE
+
+
+// CREATE ROUTE
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("blog server is running...better catch it!");
+});
